9a1df5f3694006638027290aa85595cb8f82270a
Gar <gar@lonelydatu.com>
2018-07-05 14:23:54 -0400

    one

--- dev/opinion_728x90/_js/main.js
+++ dev/opinion_728x90/_js/main.js
@@ -13,14 +13,16 @@ function start(){
 
     tl.add('f2', '-=.2')
     tl.set(".frame2", {opacity:1})
-    tl.from('#logo', .5, {opacity: 0}, 'f2')
+    tl.from('.frame2 #logo', .5, {opacity: 0}, 'f2')
     tl.from('#t3', .5, {opacity: 0}, 'f2')
     tl.to('.frame2', .5, {opacity:0}, '+=4')
 
     tl.add('f3', '+=4')
     tl.set(".frame3", {opacity:1})    
     tl.from('#medium', .5, {y:size.h})
+    tl.from('.frame3 #logo', .5, {opacity:0}, '-=.3')
     tl.from('#t4', .3, {opacity:0})
+
     tl.from('#cta', .5, {opacity:0}, '+=.3')
     
 
